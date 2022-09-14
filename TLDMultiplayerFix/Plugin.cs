18d7031a8a40275d027a91d898325b0be3b63cec
Jack Baron <jackmbaron@gmail.com>
2022-09-14 21:43:27 +0100

    define config

--- TLDMultiplayerFix/Plugin.cs
+++ TLDMultiplayerFix/Plugin.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx;
+using BepInEx;
+using BepInEx.Configuration;
 
 namespace TLDMultiplayerFix
 {
@@ -6,6 +7,13 @@ namespace TLDMultiplayerFix
     [BepInProcess("TheLongDrive.exe")]
     public class Plugin : BaseUnityPlugin
     {
+        private ConfigEntry<float> configPhysicsUpdateRate;
+
+        public Plugin()
+        {
+            configPhysicsUpdateRate = Config.Bind("Sync", "Physics.UpdateRate", 10f, "Physics update rate (Hz)");
+        }
+
         private void Awake()
         {
 
