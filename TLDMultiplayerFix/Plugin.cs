68cb7fb5745822a84e46f425494932961d1fc688
Jack Baron <jackmbaron@gmail.com>
2022-09-14 21:33:06 +0100

    only load for the long drive

--- TLDMultiplayerFix/Plugin.cs
+++ TLDMultiplayerFix/Plugin.cs
@@ -3,6 +3,7 @@
 namespace TLDMultiplayerFix
 {
     [BepInPlugin("dev.lulu.plugins.tldmpfix", "TLD Multiplayer Fix", PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("TheLongDrive.exe")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
